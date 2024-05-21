184c2da8aebb83de78d41655beb9d724f9352f75
Ramonlegend <ramongodinho01@gmail.com>
2024-05-21 09:58:08 -0300

    exercicio atualizado

--- fix61.1.py
+++ fix61.1.py
@@ -5,10 +5,12 @@
 print("RA: 1051392411014, Nome: Ramon Godinho, Turma: 1 semestre DSM ")
 
 # Register 3 email addresses
-emails = []
-for i in range(3):
-  email = input("Enter an email address: ")
-  emails.append(email)
+with open('email.txt', 'a') as file:
+  emails = []
+  for i in range(3):
+    email = input("Enter an email address: ")
+    emails.append(email)
+    file.write(email + '\n')
 
 print("Email addresses registered:")
 for email in emails:
